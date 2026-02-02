a989970f3fec777a975479a87d6cd4165a217265
vitaliiyuhulovatyi-alt <vitalii.yu.hulovatyi@ukd.edu.ua>
2026-02-02 20:56:52 +0200

    додав 5 ліній тексту

---
+++ main1.py
@@ -0,0 +1,5 @@
+print("hello world")#привіт
+print("i am 15")
+print("i love computer games")
+print("i am student ")
+print("i am programmer") 
--- python_project.py
+++
@@ -1,2 +0,0 @@
-print("hello world")
-print("i am 15")
