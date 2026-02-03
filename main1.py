cf7b21d85c4a735345338c5b7e9b836c4a51e1f8
vitaliiyuhulovatyi-alt <vitalii.yu.hulovatyi@ukd.edu.ua>
2026-02-03 11:08:13 +0200

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,5 +1,9 @@
-print("hello world")#привіт
-print("i am 15")
-print("i love computer games")
-print("i am student ")
-print("i am programmer") 
+#print("hello world")#привіт
+#print("i am 15")#
+#print("i love computer games")#
+#print("i am student ")#
+#print("i am programmer") #
+#print("123")#
+a = 10
+b = 5
+print (a + b)
